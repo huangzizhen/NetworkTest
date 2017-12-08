a667361e239568eedccf73e6e1c43087f18bc41a
huangzizhen <1264790585@qq.com>
2017-12-08 12:03:33 +0800

    添加注释

--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -14,6 +14,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+/**
+ * 你好啊，github
+ */
 public class MainActivity extends AppCompatActivity  {
       TextView responseText;
      @Override
